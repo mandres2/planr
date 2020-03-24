8bebe6819be46ae470fde249842e88ef37b28f1e
Micah Andres <limbo.linux.dev@gmail.com>
2020-03-23 21:26:10 -0700

    loaded project in real-time

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -1,21 +1,47 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { firestoreConnect } from 'react-redux-firebase'
+import { compose } from 'redux'
 
 function ProjectDetails(props) {
-    const id = props.match.params.id;
-    return (
-        <div className="container section project-details">
-            <div className="card z-depth-0">
-                <div className="card content">
-                    <span className="card-title">Project Title - {id} </span>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum dolore placeat delectus aliquam commodi nam ducimus non est fugit dignissimos distinctio quaerat corrupti facere ex totam esse voluptatem, amet aperiam.</p>
+    const { project } = props;
+    if (project) {
+        return (
+            <div className="container section project-details">
+                <div className="card z-depth-0">
+                    <div className="card content">
+                        <span className="card-title">{ project.title }</span>
+                        <p>{ project.content }</p>
+                    </div>
+                <div className="card-action grey-lighten-4 grey-text">
+                    <div>Posted by { project.authorFirstName } { project.authorLastName } </div>
+                    <div>22nd March, 1530</div>
                 </div>
-            <div className="card-action grey-lighten-4 grey-text">
-                <div>Posted by LimboLinux</div>
-                <div>22nd March, 1530</div>
+                </div>
             </div>
+        )
+    } else {
+        return (
+            <div className="container center">
+                <p style= {{ color: 'white' }}>Please wait. Loading Project.</p>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
-export default ProjectDetails
+const mapStateToProps = (state, ownProps) => {
+    // console.log(state);
+    const id = ownProps.match.params.id;
+    const projects = state.firestore.data.projects;
+    const project = projects ? projects[id] : null
+    return {
+        project: project
+    }
+}
+
+export default compose(
+    connect(mapStateToProps),
+    firestoreConnect([
+        { collection: 'projects'}
+    ])
+)(ProjectDetails)
--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -2,13 +2,16 @@
 import React from 'react'
 // Utilizing this functional component makes this portion a lot more modular rather than having to re-write the code.
 import ProjectSummary from './ProjectSummary'
+import { Link } from 'react-router-dom'
 
 const ProjectList = ({projects}) => {
     return (
         <div className="project-list section">
             { projects && projects.map(project => {
                 return (
-                    <ProjectSummary project={project} key={project.id} />
+                    <Link to = { '/project/' + project.id } >
+                        <ProjectSummary project={project} key={project.id} />
+                    </Link>
                 )
             })}
         </div>
