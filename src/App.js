e38d2d48e5dbb47b6c1fd93fc33d79658c316337
Micah Andres <limbo.linux.dev@gmail.com>
2020-03-30 22:09:48 -0700

    implemented user-signup functionlaity with Firebase Auth @ 2209 -- operating application

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import Dashboard from './components/dashboard/Dashboard'
 import ProjectDetails from './components/projects/ProjectDetails'
-import SignIn from './components/auth/SignedIn'
-import SignUp from './components/auth/SignedUp'
+import SignIn from './components/auth/SignIn'
+import SignUp from './components/auth/SignUp'
 import CreateProject from './components/projects/CreateProject'
 
 function App() {
--- src/store/actions/authActions.js
+++ src/store/actions/authActions.js
@@ -22,3 +22,27 @@ export const signOut = () => {
         });
     }
 }
+
+export const signUp = (newUser) => {
+    return (dispatch, getState, {getFirebase, getFirestore}) => {
+        const firebase = getFirebase();
+        const firestore = getFirestore();
+
+        firebase.auth().createUserWithEmailAndPassword(
+            newUser.email,
+            newUser.password
+            // The following lines below creates the newUser Record.
+        ).then((resp) => {
+            // response.user.uid will get the data from the newUser and storing it in this document thus returning a promise based upon Lines 37-39
+            return firestore.collection('users').doc(resp.user.uid).set({
+                firstName: newUser.firstName,
+                lastName: newUser.lastName,
+                initials: newUser.firstName[0] + newUser.lastName[0]
+            })
+        }).then(() => {
+            dispatch({ type: 'SIGNUP_SUCCESS' })
+        }).catch(err => {
+            dispatch ({ type: 'SIGNUP_ERROR', err })
+        })
+    }
+}
